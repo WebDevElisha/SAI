b830bcd3f5b8c9011030a9dc0ecc1626beec46b0
WebDevElisha <ElishaCreatesWebs1@gmail.com>
2025-08-01 10:29:41 -0400

    Update SaiAI.js

--- SaiAI.js
+++ SaiAI.js
@@ -1,1 +1,24 @@
+import { detectSupportMood } from './SaiSupportTrigger.js';
+import { supportVault } from './SaiSupportVault.js';
+import { detectMoodTone } from './SaiMoodTrigger.js';
+import { moodVault } from './SaiMoodVault.js';
+
+export function respondToMessage(userMessage) {
+  const supportMood = detectSupportMood(userMessage);
+  const moodTone = detectMoodTone(userMessage);
+
+  if (supportMood && supportVault[supportMood]) {
+    return randomLine(supportVault[supportMood]);
+  }
+
+  if (moodTone && moodVault[moodTone]) {
+    return randomLine(moodVault[moodTone]);
+  }
+
+  return standardResponseFlow(userMessage);
+}
+
+function randomLine(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
